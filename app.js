fb839c29743651f42f1ab7d52a0146f677ada978
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-05-25 16:05:49 -0700

    converted keypress eventlistener to arrow function

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ puzzleElement.textContent = game1.puzzle;
 guessesElement.textContent = game1.statusMessage;
 console.log(game1.status);
 
-window.addEventListener('keypress', function(e) {
+window.addEventListener('keypress', e => {
   const guess = String.fromCharCode(e.charCode);
   game1.makeGuess(guess);
   puzzleElement.textContent = game1.puzzle;
