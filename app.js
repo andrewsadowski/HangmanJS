a1862a5fc4c3736389480d8452c491a12194b5ef
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-05-25 15:52:02 -0700

    created xhr request for random word

--- app.js
+++ app.js
@@ -12,3 +12,15 @@ window.addEventListener('keypress', function(e) {
   puzzleElement.textContent = game1.puzzle;
   guessesElement.textContent = game1.statusMessage;
 });
+
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', e => {
+  if (e.target.readyState === 4) {
+    const data = JSON.parse(e.target.responseText);
+    console.log(data);
+  }
+});
+
+request.open('GET', 'http://puzzle.mead.io/puzzle');
+request.send();
