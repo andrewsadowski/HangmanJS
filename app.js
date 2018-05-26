7f1afa211c1880ef3356e39b7f4dde9cc7abe308
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-05-25 18:59:06 -0700

    added callback to distribute puzzle word

--- app.js
+++ app.js
@@ -13,30 +13,38 @@ window.addEventListener('keypress', e => {
   guessesElement.textContent = game1.statusMessage;
 });
 
-const request = new XMLHttpRequest();
-
-request.addEventListener('readystatechange', e => {
-  if (e.target.readyState === 4 && e.target.status === 200) {
-    const data = JSON.parse(e.target.responseText);
-    console.log(data);
-  } else if (e.target.readyState === 4) {
-    console.log('An error has occured');
+getPuzzle((error, puzzle) => {
+  if (error) {
+    console.log(`Error: ${error}`);
+  } else {
+    console.log(puzzle);
   }
 });
 
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3');
-request.send();
+// const request = new XMLHttpRequest();
 
-const countryCode = 'US';
-const countryRequest = new XMLHttpRequest();
+// request.addEventListener('readystatechange', e => {
+//   if (e.target.readyState === 4 && e.target.status === 200) {
+//     const data = JSON.parse(e.target.responseText);
+//     console.log(data);
+//   } else if (e.target.readyState === 4) {
+//     console.log('An error has occured');
+//   }
+// });
 
-countryRequest.addEventListener('readystatechange', e => {
-  if (e.target.readyState === 4 && e.target.status === 200) {
-    const data = JSON.parse(e.target.responseText);
-    const countryFinder = data.find(country => country.alpha2Code === countryCode);
-    console.log(countryFinder.name);
-  }
-});
+// request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3');
+// request.send();
 
-countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
-countryRequest.send();
+// const countryCode = 'US';
+// const countryRequest = new XMLHttpRequest();
+
+// countryRequest.addEventListener('readystatechange', e => {
+//   if (e.target.readyState === 4 && e.target.status === 200) {
+//     const data = JSON.parse(e.target.responseText);
+//     const countryFinder = data.find(country => country.alpha2Code === countryCode);
+//     console.log(countryFinder.name);
+//   }
+// });
+
+// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
+// countryRequest.send();
